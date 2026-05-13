539da605c11043befddf863176d057da5c229cd1
Valetish <meowciry@gmail.com>
2026-05-14 00:03:26 +0200

    Write the logic to get the computer choice

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("Hello, World!");
+
+//Step 2: Write the logic to get the computer choice
+function getComputerChoice(){
+    return Math.floor(Math.random() * 3);
+}
+console.log(getComputerChoice());
+
